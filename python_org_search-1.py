3bdb44357f9ba79b1954541365a789d7e5e5f49d
Josue23 <josuerodrigues@gmail.com>
2017-01-26 23:34:19 -0200

    python_org_search.py

---
+++ python_org_search-1.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+# https://selenium-python.readthedocs.io/getting-started.html
+
+# The selenium.webdriver module provides all the WebDriver implementations.
+# Currently supported WebDriver implementations are Firefox, Chrome, IE and Remote.
+from selenium import webdriver
+
+# The Keys class provide keys in the keyboard like RETURN, F1, ALT etc.
+from selenium.webdriver.common.keys import Keys
+
+# Next, the instance of Firefox WebDriver is created.
+driver = webdriver.Firefox()
+
+# The driver.get method will navigate to a page given by the URL
+driver.get("http://www.python.org")
+
+# The next line is an assertion to confirm that title has “Python” word in it:
+assert "Python" in driver.title
+
+#  the input text element can be located by its name attribute using find_element_by_name method
+elem = driver.find_element_by_name("q")
+
+#  To be safe, we’ll first clear any prepopulated text in the input field
+#  (e.g. “Search”) so it doesn’t affect our search results:
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+
+# After submission of the page, you should get the result if there is any.
+# To ensure that some results are found, make an assertion:
+assert "No results found." not in driver.page_source
+
+
+driver.close()
+'''
+
+# o mesmo exemplo, usando unittest
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Inheriting from TestCase class is the way to tell unittest module
+# that this is a test case
+class PythonOrgSearch(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_search_in_python_org(self):
+        driver = self.driver
+        driver.get("http://www.python.org")
+        self.assertIn("Python", driver.title)
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ python_org_search.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# https://selenium-python.readthedocs.io/getting-started.html
+
+# The selenium.webdriver module provides all the WebDriver implementations.
+# Currently supported WebDriver implementations are Firefox, Chrome, IE and Remote.
+from selenium import webdriver
+
+# The Keys class provide keys in the keyboard like RETURN, F1, ALT etc.
+from selenium.webdriver.common.keys import Keys
+
+# Next, the instance of Firefox WebDriver is created.
+driver = webdriver.Firefox()
+
+# The driver.get method will navigate to a page given by the URL
+driver.get("http://www.python.org")
+
+# The next line is an assertion to confirm that title has “Python” word in it:
+assert "Python" in driver.title
+
+#  the input text element can be located by its name attribute using find_element_by_name method
+elem = driver.find_element_by_name("q")
+
+#  To be safe, we’ll first clear any prepopulated text in the input field
+#  (e.g. “Search”) so it doesn’t affect our search results:
+elem.clear()
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+
+# After submission of the page, you should get the result if there is any.
+# To ensure that some results are found, make an assertion:
+assert "No results found." not in driver.page_source
+
+
+driver.close()
