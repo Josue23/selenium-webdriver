5dfdde5486807c1f6cc7ae799b689708749b42c6
Josue23 <josuerodrigues@gmail.com>
2017-03-02 22:57:16 -0300

    adicionado a pasta testGroupBDD/

---
+++ testGroupBDD/steps/steps.py
@@ -0,0 +1,9 @@
+from behave import given, when, then
+
+@given("I am the main directory")
+def in_main_dir(context):
+    pass
+
+@then("I should also be in the main directory")
+def also_in_main_dir(context):
+    pass
---
+++ testGroupBDD/test_case_1.feature
@@ -0,0 +1,6 @@
+Feature: Test Cases Group 1
+
+Scenario: Running Teste Case 1
+
+        Given I am the main directory
+        Then I should also be in the main directory
